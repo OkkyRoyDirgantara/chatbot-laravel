0e5ae6e8145a1b8ed92e6b41e7feebd7efb92328
Okky Roy Dirgantara <okkyroydirgantara@gmail.com>
2023-04-30 12:25:36 +0700

    Add Meta Data

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -4,6 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Mitigasi Banjir | @yield('title')</title>
+
+    <meta name="description" content="Pembuatan Chatbot untuk Mitigasi dan Evakuasi Banjir di Lamongan">
+    <meta name="author" content="Okky Roy Dirgantara">
+    <meta name="keywords" content="chatbot, telegram, mitigasi, evakuasi, banjir, lamongan">
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <meta name="google" content="notranslate">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
     <style>
