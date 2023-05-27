562e7c4c43e50d4d1b154e313ddb72d270fd4a2d
Okky Roy Dirgantara <okkyroydirgantara@gmail.com>
2023-05-27 08:00:16 +0700

    fix error when offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,9 +13,47 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-        fetch(event.request).catch(function() {
+        fetch(event.request).catch(function(error) {
             // Tangani kesalahan jaringan
             console.error('Error fetching:', error);
+
+            // Membuat tampilan HTML pesan kesalahan
+            var errorResponse = `
+        <html>
+          <head>
+            <title>Network Error</title>
+            <style>
+              body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 20px;
+                background-color: #f8f8f8;
+              }
+              h1 {
+                color: #333;
+                font-size: 24px;
+              }
+              p {
+                color: #666;
+                font-size: 16px;
+              }
+            </style>
+          </head>
+          <body>
+            <h1>Network Error</h1>
+            <p>There was a network error. Please try again later.</p>
+          </body>
+        </html>
+      `;
+
+            // Membuat respons dengan tampilan HTML pesan kesalahan
+            var response = new Response(errorResponse, {
+                status: 500,
+                statusText: 'Internal Server Error',
+                headers: { 'Content-Type': 'text/html' }
+            });
+
+            return response;
         })
     );
 });
